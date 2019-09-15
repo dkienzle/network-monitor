286756f291adb28705859b9fe212090963648027
Darrell Kienzle <kienzle@oldAir.local>
2019-09-15 11:53:42 -0400

    some basic cleanup and gofmt

--- dnssniff/dnssniff.go
+++ dnssniff/dnssniff.go
@@ -1,39 +1,39 @@
-
 package main
 
 import (
-   "fmt"
-   "github.com/google/gopacket"
-   "github.com/google/gopacket/pcap"
-   "github.com/google/gopacket/layers"
+	"fmt"
 	"log"
-   "time"
 	"net"
 	"os"
 	"sort"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
 // globals.  from the pcap sample code.  except they stupidly the len to 1024 which caused all sorts of decoding errors
 var (
-    device       string = "en0"
-    snapshot_len int32  = 1500
-    promiscuous  bool   = false
-    err          error
-    timeout      time.Duration = 30 * time.Second
-    handle       *pcap.Handle
+	device       string = "en0"
+	snapshot_len int32  = 1500
+	promiscuous  bool   = false
+	err          error
+	timeout      time.Duration = 30 * time.Second
+	handle       *pcap.Handle
 )
 
-
 type Device struct {
-	MAC       string
+	MAC          string
 	manufacturer string
 	IPv4         net.IP
-	clientID  string
-	NBNames map[string]int
-	dnsnames map[string]uint64
+	clientID     string
+	NBNames      map[string]int
+	dnsnames     map[string]uint64
 }
 
 var Devices map[string]*Device
+
 // every function named init() is called at load time to initialize globals
 func init() {
 	Devices = make(map[string]*Device)
@@ -51,7 +51,6 @@ func (a ByIP) Len() int           { return len(a) }
 func (a ByIP) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByIP) Less(i, j int) bool { return string(a[i].IPv4) < string(a[j].IPv4) }
 
-
 func min(a, b int) int {
 	if a < b {
 		return a
@@ -63,29 +62,31 @@ func min(a, b int) int {
 // read a file if a name is given, otherwise set up listening to en0
 func main() {
 
-	if (len(os.Args) > 1) {
+	if len(os.Args) > 1 {
 		fname := os.Args[1]
 		handle, err = pcap.OpenOffline(fname)
-      if err != nil {
-         log.Fatal("PCAP OpenOffline error (handle to read packet):", err)
-      }
+		if err != nil {
+			log.Fatal("PCAP OpenOffline error (handle to read packet):", err)
+		}
 	} else {
-		handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)		
+		handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
 
-		if err != nil {log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	defer handle.Close()
 
 	// Set filter
-//	var filter string = "ether multicast"
-//	err = handle.SetBPFFilter(filter)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	fmt.Println("Only capturing multicast packets.")
-	
+	//	var filter string = "ether multicast"
+	//	err = handle.SetBPFFilter(filter)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	fmt.Println("Only capturing multicast packets.")
+
 	layers.RegisterUDPPortLayerType(5353, layers.LayerTypeDNS)
-		
+
 	// Use the handle as a packet source to process all packets
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
@@ -103,25 +104,25 @@ func writeSummary() {
 	// For now sort by MAC address since that's easiest, since they are constant over the years, and since many systems have
 	// more than one MAC that are close together
 	sorted := make([]*Device, len(Devices))
-   i := 0
+	i := 0
 
-   for  _, entry := range Devices {
-      sorted[i] = entry
-      i++
-   }
+	for _, entry := range Devices {
+		sorted[i] = entry
+		i++
+	}
 
-//   sort.Sort(ByMAC(sorted))
-   sort.Sort(ByIP(sorted))
-	
+	//   sort.Sort(ByMAC(sorted))
+	sort.Sort(ByIP(sorted))
+
 	fmt.Println()
-	for _,d := range sorted {
+	for _, d := range sorted {
 		fmt.Println()
-		fmt.Printf("%20s %20s %20s\n",d.MAC, d.clientID, d.IPv4)
-		for n,u := range d.dnsnames {
-			fmt.Printf("%d\t%s\n",u,n)
+		fmt.Printf("%20s %20s %20s\n", d.MAC, d.clientID, d.IPv4)
+		for n, u := range d.dnsnames {
+			fmt.Printf("%d\t%s\n", u, n)
 		}
 	}
-	
+
 }
 
 // Retrieve the device record for this MAC address.
@@ -137,7 +138,7 @@ func getDevice(mac net.HardwareAddr) *Device {
 	newDevice.NBNames = make(map[string]int)
 	newDevice.dnsnames = make(map[string]uint64)
 	newDevice.MAC = mac.String()
-	
+
 	Devices[string(mac)] = &newDevice
 	return &newDevice
 }
@@ -148,39 +149,38 @@ func printPacketInfo(packet gopacket.Packet) {
 
 	if ethernetLayer == nil {
 		fmt.Println("No ethernet layer detected!")
-		return 
+		return
 	}
-			
+
 	ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
 
 	srcMAC := ethernetPacket.SrcMAC.String()
-   d := getDevice(ethernetPacket.SrcMAC)
-	
+	d := getDevice(ethernetPacket.SrcMAC)
+
 	dstMAC := ethernetPacket.DstMAC.String()
 
-//	fmt.Println("Ethernet packet: ",srcMAC,"   ",dstMAC)
-//	fmt.Printf("Ethernet type: %d, %s\n", ethernetPacket.EthernetType,ethernetPacket.EthernetType)
+	//	fmt.Println("Ethernet packet: ",srcMAC,"   ",dstMAC)
+	//	fmt.Printf("Ethernet type: %d, %s\n", ethernetPacket.EthernetType,ethernetPacket.EthernetType)
 	switch ethernetPacket.EthernetType {
 
 	case layers.EthernetTypeIPv4:
-//		fmt.Println("Found an IPv4")
+		//		fmt.Println("Found an IPv4")
 		PrintIPv4Info(d, packet, srcMAC, dstMAC)
 
 	case layers.EthernetTypeARP:
 		arpLayer := packet.Layer(layers.LayerTypeARP)
 		arp, _ := arpLayer.(*layers.ARP)
-		
+
 		if len(d.IPv4) == 0 || d.IPv4.IsUnspecified() {
 			d.IPv4 = net.IP(arp.SourceProtAddress)
 		}
 
 	default:
-		fmt.Printf("UNKNOWN PACKET FROM %s to %s\n",srcMAC, dstMAC)
+		fmt.Printf("UNKNOWN PACKET FROM %s to %s\n", srcMAC, dstMAC)
 	}
 
 	return
 }
-
 
 func dumpDNS(d *Device, packet gopacket.Packet, request bool) bool {
 	dnsLayer := packet.Layer(layers.LayerTypeDNS)
@@ -208,7 +208,7 @@ func dumpDNS(d *Device, packet gopacket.Packet, request bool) bool {
 	}
 
 	return true
-}		
+}
 
 func PrintIPv4Info(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 	ipLayer := packet.Layer(layers.LayerTypeIPv4)
@@ -217,33 +217,32 @@ func PrintIPv4Info(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 	if !ip.SrcIP.IsUnspecified() {
 		d.IPv4 = ip.SrcIP
 	}
-	
+
 	switch ip.Protocol {
 
 	case layers.IPProtocolUDP:
-	
+
 		dhcpLayer := packet.Layer(layers.LayerTypeDHCPv4)
 		if dhcpLayer != nil {
 			dhcp, _ := dhcpLayer.(*layers.DHCPv4)
 
 			hostname := ""
-			for _,o := range dhcp.Options {
+			for _, o := range dhcp.Options {
 				if o.Type == layers.DHCPOptHostname {
 					hostname = string(o.Data)
 				}
 			}
 
-			fmt.Printf("DHCP %s ",dhcp.Operation)
+			fmt.Printf("DHCP %s ", dhcp.Operation)
 			fmt.Printf("from %s / %s to %s ", ip.SrcIP, srcMAC, ip.DstIP)
 			fmt.Printf("hostname: %s\n", hostname)
 
-			
 			if hostname == "" {
-				fmt.Println(" %q",dhcp.Options)
+				fmt.Println(" %q", dhcp.Options)
 			} else {
 				d.clientID = hostname
 			}
-			
+
 			return
 		}
 
@@ -253,7 +252,7 @@ func PrintIPv4Info(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 
 			// if we have never seen traffic to this port from this system before, dump the packat
 			if udp.DstPort == 53 {
-				if !dumpDNS(d,packet, true) {
+				if !dumpDNS(d, packet, true) {
 					applicationLayer := packet.ApplicationLayer()
 					if applicationLayer != nil {
 						Hexdump(applicationLayer.Payload())
@@ -263,21 +262,21 @@ func PrintIPv4Info(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 				}
 			}
 			if udp.SrcPort == 53 {
-				dumpDNS(d,packet,false)
+				dumpDNS(d, packet, false)
 			}
 
 		}
 		return
 
 	case layers.IPProtocolTCP:
-		
+
 		// Honestly, for multicast, we should NEVER see TCP...
 		tcpLayer := packet.Layer(layers.LayerTypeTCP)
 		if tcpLayer != nil {
 			tcp, _ := tcpLayer.(*layers.TCP)
 
 			if tcp.DstPort == 53 {
-				if !dumpDNS(d,packet, true) {
+				if !dumpDNS(d, packet, true) {
 					applicationLayer := packet.ApplicationLayer()
 					if applicationLayer != nil {
 						Hexdump(applicationLayer.Payload())
@@ -296,7 +295,6 @@ func PrintIPv4Info(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 	}
 }
 
-
 func DumpUnknown(packet gopacket.Packet) {
 
 	fmt.Println("All packet layers:")
@@ -310,33 +308,33 @@ func DumpUnknown(packet gopacket.Packet) {
 }
 
 func Hexdump(data []byte) {
-        for i := 0; i < len(data); i += 16 {
-                Dumpline(uint32(i), data[i:min(i+16, len(data))])
-        }
+	for i := 0; i < len(data); i += 16 {
+		Dumpline(uint32(i), data[i:min(i+16, len(data))])
+	}
 }
 
 func Dumpline(addr uint32, line []byte) {
-        fmt.Printf( "\t0x%04x: ", int32(addr))
-        var i uint16
-        for i = 0; i < 16 && i < uint16(len(line)); i++ {
-                if i%2 == 0 {
-                        os.Stdout.WriteString(" ")
-                }
-                fmt.Printf( "%02x", line[i])
-        }
-        for j := i; j <= 16; j++ {
-                if j%2 == 0 {
-                        os.Stdout.WriteString(" ")
-                }
-                os.Stdout.WriteString("  ")
-        }
-        os.Stdout.WriteString("  ")
-        for i = 0; i < 16 && i < uint16(len(line)); i++ {
-                if line[i] >= 32 && line[i] <= 126 {
-                        fmt.Printf("%c", line[i])
-                } else {
-                        os.Stdout.WriteString(".")
-                }
-        }
-        os.Stdout.WriteString("\n")
+	fmt.Printf("\t0x%04x: ", int32(addr))
+	var i uint16
+	for i = 0; i < 16 && i < uint16(len(line)); i++ {
+		if i%2 == 0 {
+			os.Stdout.WriteString(" ")
+		}
+		fmt.Printf("%02x", line[i])
+	}
+	for j := i; j <= 16; j++ {
+		if j%2 == 0 {
+			os.Stdout.WriteString(" ")
+		}
+		os.Stdout.WriteString("  ")
+	}
+	os.Stdout.WriteString("  ")
+	for i = 0; i < 16 && i < uint16(len(line)); i++ {
+		if line[i] >= 32 && line[i] <= 126 {
+			fmt.Printf("%c", line[i])
+		} else {
+			os.Stdout.WriteString(".")
+		}
+	}
+	os.Stdout.WriteString("\n")
 }
--- listen/listener.go
+++ listen/listener.go
@@ -1,40 +1,29 @@
-
 package main
 
 import (
-   "fmt"
-   "github.com/google/gopacket"
-   "github.com/google/gopacket/pcap"
-   "github.com/google/gopacket/macs"
-   "github.com/google/gopacket/layers"
-	"log"
 	"bytes"
-   "time"
+	"fmt"
+	"log"
 	"net"
 	"os"
-	"strconv"
 	"sort"
+	"strconv"
+	"time"
 	"unicode"
-)
 
-// globals.  from the pcap sample code.  except they stupidly the len to 1024 which caused all sorts of decoding errors
-var (
-    device       string = "en0"
-    snapshot_len int32  = 1500
-    promiscuous  bool   = false
-    err          error
-    timeout      time.Duration = 30 * time.Second
-    handle       *pcap.Handle
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/macs"
+	"github.com/google/gopacket/pcap"
 )
 
 type PortSet map[int]bool
-
 
 func (p PortSet) String() string {
 	var retval bytes.Buffer
 	delim := ""
 
-	for port,_ := range p {
+	for port, _ := range p {
 		retval.WriteString(delim)
 		retval.WriteString(strconv.Itoa(port))
 		delim = ","
@@ -43,30 +32,27 @@ func (p PortSet) String() string {
 }
 
 type Device struct {
-	MAC       string
+	MAC          string
 	manufacturer string
 	IPv4         net.IP
-	clientID  string
-	NBNames map[string]int
-	IPv6    net.IP
-	packets   uint64
-	ports PortSet
-	snap bool
-	WOMP bool
-	bonjourQ map[string]bool
-	bonjourA map[string][]byte
-	bonjourB map[string][]byte
-	bonjourC map[string][]byte
-   // need some sort of timestamp
+	clientID     string
+	NBNames      map[string]int
+	IPv6         net.IP
+	packets      uint64
+	ports        PortSet
+	snap         bool
+	WOMP         bool
+	bonjourQ     map[string]bool
+	bonjourA     map[string][]byte
+	bonjourB     map[string][]byte
+	bonjourC     map[string][]byte
+	// need some sort of timestamp
 	// need some sort of activity total.... possibly with decay
 }
 
-var Devices map[string]*Device
+type DeviceList map[string]*Device
 
 // every function named init() is called at load time to initialize globals
-func init() {
-	Devices = make(map[string]*Device)
-}
 
 type ByMAC []*Device
 
@@ -80,7 +66,6 @@ func (a ByIP) Len() int           { return len(a) }
 func (a ByIP) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByIP) Less(i, j int) bool { return string(a[i].IPv4) < string(a[j].IPv4) }
 
-
 func min(a, b int) int {
 	if a < b {
 		return a
@@ -92,16 +77,27 @@ func min(a, b int) int {
 // read a file if a name is given, otherwise set up listening to en0
 func main() {
 
-	if (len(os.Args) > 1) {
+	devices := make(DeviceList) //make(map[string]*Device)
+
+	device := "en0"
+	snapshotLen := int32(1500)
+	promiscuous := false
+	timeout := 30 * time.Second
+	var handle *pcap.Handle
+	var err error
+
+	if len(os.Args) > 1 {
 		fname := os.Args[1]
 		handle, err = pcap.OpenOffline(fname)
-      if err != nil {
-         log.Fatal("PCAP OpenOffline error (handle to read packet):", err)
-      }
+		if err != nil {
+			log.Fatal("PCAP OpenOffline error (handle to read packet):", err)
+		}
 	} else {
-		handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)		
+		handle, err = pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
 
-		if err != nil {log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	defer handle.Close()
 
@@ -112,82 +108,81 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Only capturing multicast packets.")
-	
+
 	layers.RegisterUDPPortLayerType(5353, layers.LayerTypeDNS)
-		
+
 	// Use the handle as a packet source to process all packets
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
 	for packet := range packetSource.Packets() {
 		// Process packet here
-		printPacketInfo(packet)
+		devices.recordPacketInfo(packet)
 	}
 
-	writeSummary()
+	devices.writeSummary()
 }
 
 func (d *Device) bump() {
 	d.packets++
 }
 
-func writeSummary() {
+func (devices DeviceList) writeSummary() {
 
-	// Go randomizes the iterator order for a map on purpose.  So to get them into a consistent order, we need to sort first.
-	// For now sort by MAC address since that's easiest, since they are constant over the years, and since many systems have
-	// more than one MAC that are close together
-	sorted := make([]*Device, len(Devices))
-   i := 0
+	// Go randomizes the iterator order for a map on purpose.  So to get them into a consistent order,
+	// we need to sort first.  For now sort by MAC address since that's easiest, since they are constant
+	// over the years, and since many systems have more than one MAC that are close together
+	sorted := make([]*Device, len(devices))
+	i := 0
 
-   for  _, entry := range Devices {
-      sorted[i] = entry
-      i++
-   }
-
-//   sort.Sort(ByMAC(sorted))
-   sort.Sort(ByIP(sorted))
-	
-	fmt.Println()
-	for _,d := range sorted {
-		fmt.Println()
-		fmt.Printf("%20s %20s %20s %30s %30s %8d %t\n",d.MAC, d.clientID, d.IPv4, d.IPv6, d.manufacturer, d.packets, d.snap)
-		fmt.Println("\tUDP Ports:",d.ports)
-		for n,c := range d.NBNames {
-			fmt.Printf("\tNetBIOS (%d) = %s\n",c,n)
-		}
-		for n,_ := range d.bonjourQ {
-			fmt.Println("\tQ:",n)
-		}
-		for n,_ := range d.bonjourA {
-			fmt.Println("\tA:",n)
-			//Hexdump(x)
-		}
-		for n,_ := range d.bonjourB {
-			fmt.Println("\tB:",n)
-			//Hexdump(x)
-		}
-		for n,_ := range d.bonjourC {
-			fmt.Println("\tC:",n)
-			//Hexdump(x)
-		}		
+	for _, entry := range devices {
+		sorted[i] = entry
+		i++
 	}
-	
+
+	//   sort.Sort(ByMAC(sorted))
+	sort.Sort(ByIP(sorted))
+
+	fmt.Println()
+	for _, d := range sorted {
+		fmt.Println()
+		fmt.Printf("%20s %20s %20s %30s %30s %8d %t\n", d.MAC, d.clientID, d.IPv4, d.IPv6, d.manufacturer, d.packets, d.snap)
+		fmt.Println("\tUDP Ports:", d.ports)
+		for n, c := range d.NBNames {
+			fmt.Printf("\tNetBIOS (%d) = %s\n", c, n)
+		}
+		for n, _ := range d.bonjourQ {
+			fmt.Println("\tQ:", n)
+		}
+		for n, _ := range d.bonjourA {
+			fmt.Println("\tA:", n)
+			//Hexdump(x)
+		}
+		for n, _ := range d.bonjourB {
+			fmt.Println("\tB:", n)
+			//Hexdump(x)
+		}
+		for n, _ := range d.bonjourC {
+			fmt.Println("\tC:", n)
+			//Hexdump(x)
+		}
+	}
+
 }
 
 func getOrgName(mac []byte) string {
-	var prefix [3]byte 
+	var prefix [3]byte
 	prefix[0] = mac[0]
 	prefix[1] = mac[1]
 	prefix[2] = mac[2]
-	
-	return macs.ValidMACPrefixMap[prefix]		
-}
 
+	return macs.ValidMACPrefixMap[prefix]
+}
 
 // Retrieve the device record for this MAC address.
 // If this is the first time we have seen this MAC,
 // create a new record and _initialize_ the fields!
-func getDevice(mac net.HardwareAddr) *Device {
-	d, ok := Devices[string(mac)]
+func (devices DeviceList) getDevice(mac net.HardwareAddr) *Device {
+	d, ok := devices[string(mac)]
 	if ok {
 		return d
 	}
@@ -200,38 +195,38 @@ func getDevice(mac net.HardwareAddr) *Device {
 	newDevice.bonjourB = make(map[string][]byte)
 	newDevice.bonjourC = make(map[string][]byte)
 	newDevice.MAC = mac.String()
-	
+
 	newDevice.manufacturer = getOrgName(mac)
-	Devices[string(mac)] = &newDevice
+	devices[string(mac)] = &newDevice
 	return &newDevice
 }
 
-func printPacketInfo(packet gopacket.Packet) {
+func (devices *DeviceList) recordPacketInfo(packet gopacket.Packet) {
 	// Let's see if the packet is an ethernet packet
 	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
 
 	if ethernetLayer == nil {
 		fmt.Println("No ethernet layer detected!")
-		return 
+		return
 	}
-			
+
 	ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
 
 	srcMAC := ethernetPacket.SrcMAC.String()
-   d := getDevice(ethernetPacket.SrcMAC)
+	d := devices.getDevice(ethernetPacket.SrcMAC)
 	d.bump()
-	
+
 	dstMAC := ethernetPacket.DstMAC.String()
 
-//	fmt.Println("Ethernet packet: ",srcMAC,"   ",dstMAC)
-//	fmt.Printf("Ethernet type: %d, %s\n", ethernetPacket.EthernetType,ethernetPacket.EthernetType)
+	//	fmt.Println("Ethernet packet: ",srcMAC,"   ",dstMAC)
+	//	fmt.Printf("Ethernet type: %d, %s\n", ethernetPacket.EthernetType,ethernetPacket.EthernetType)
 	switch ethernetPacket.EthernetType {
 
 	case layers.EthernetTypeLLC:
 		dumpLLC(d, packet, srcMAC, dstMAC)
-		
+
 	case layers.EthernetTypeIPv4:
-//		fmt.Println("Found an IPv4")
+		//		fmt.Println("Found an IPv4")
 		PrintIPv4Info(d, packet, srcMAC, dstMAC)
 
 	case layers.EthernetTypeIPv6:
@@ -245,10 +240,10 @@ func printPacketInfo(packet gopacket.Packet) {
 	case layers.EthernetTypeARP:
 		arpLayer := packet.Layer(layers.LayerTypeARP)
 		arp, _ := arpLayer.(*layers.ARP)
-		
-//		fmt.Printf("ARP %s ",arp.Operation)
-//		fmt.Printf("from  %s / %s / %s ",srcMAC, net.IP(arp.SourceProtAddress), d.manufacturer)
-//		fmt.Printf("to %s / %s\n",dstMAC, net.IP(arp.DstProtAddress))
+
+		//		fmt.Printf("ARP %s ",arp.Operation)
+		//		fmt.Printf("from  %s / %s / %s ",srcMAC, net.IP(arp.SourceProtAddress), d.manufacturer)
+		//		fmt.Printf("to %s / %s\n",dstMAC, net.IP(arp.DstProtAddress))
 		// for devices that aren't telling us there IP any other way...
 
 		if len(d.IPv4) == 0 || d.IPv4.IsUnspecified() {
@@ -257,15 +252,14 @@ func printPacketInfo(packet gopacket.Packet) {
 
 	case 0x0842:
 		dumpWOMP(d, srcMAC, packet)
-		
+
 	default:
-		fmt.Printf("UNKNOWN PACKET FROM %s to %s\n",srcMAC, dstMAC)
+		fmt.Printf("UNKNOWN PACKET FROM %s to %s\n", srcMAC, dstMAC)
 		Hexdump(packet.Data())
 	}
 
 	return
 }
-
 
 func dumpWOMP(d *Device, srcMAC string, packet gopacket.Packet) {
 	//WOL packet
@@ -276,8 +270,8 @@ func dumpWOMP(d *Device, srcMAC string, packet gopacket.Packet) {
 	// [20-25] dst MAC
 
 	p := packet.Data()[20:]
-	dstMAC := fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x",p[0],p[1],p[2],p[3],p[4],p[5])
-	fmt.Printf("WOMP packet from %s to %s\n",srcMAC,dstMAC)
+	dstMAC := fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", p[0], p[1], p[2], p[3], p[4], p[5])
+	fmt.Printf("WOMP packet from %s to %s\n", srcMAC, dstMAC)
 	d.WOMP = true
 }
 
@@ -289,28 +283,28 @@ func dumpWOMP(d *Device, srcMAC string, packet gopacket.Packet) {
 func extractNetBiosName(b []byte) (string, int) {
 	// if the packet is too short to contain a single question, bail
 	if len(b) < 13 {
-		return "",0
+		return "", 0
 	}
 	// if there are no questions, bail
 	if (b[4] == 0) && (b[5] == 0) {
-		return "",0
+		return "", 0
 	}
 
 	var name bytes.Buffer
 	length := int(b[12])
-	fmt.Println("length = ",length)
-	for i := 13; i < 13 + length; i+=2 {
-		char := rune((b[i]-65)*16+b[i+1]-65)
+	fmt.Println("length = ", length)
+	for i := 13; i < 13+length; i += 2 {
+		char := rune((b[i]-65)*16 + b[i+1] - 65)
 		if unicode.IsPrint(char) {
 			name.WriteString(string(char))
 			//		fmt.Println("name = ",name.String())
 		}
 	}
-	// 0x2910 is the flags for Registation broadcast, allow recursion 
+	// 0x2910 is the flags for Registation broadcast, allow recursion
 	//if (b[2] != 41) || (b[3] != 16) {
 	//	return ""
 	//}
-	return name.String(), (int(b[2])*256)+int(b[3])
+	return name.String(), (int(b[2]) * 256) + int(b[3])
 
 }
 
@@ -342,8 +336,7 @@ func dumpBonjour(d *Device, packet gopacket.Packet) bool {
 	}
 
 	return true
-}		
-
+}
 
 func dumpLLC(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 	llcLayer := packet.Layer(layers.LayerTypeLLC)
@@ -354,15 +347,15 @@ func dumpLLC(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 		Hexdump(packet.Data())
 		return
 	}
-	
+
 	snapLayer := packet.Layer(layers.LayerTypeSNAP)
 	snap, ok := snapLayer.(*layers.SNAP)
 
-	fmt.Printf("LLC/SNAP packet from %s to %s \n",srcMAC, dstMAC)
-	fmt.Printf("\tDSAP: %d SSAP:%d CR:%t IG:%t  Control: %d\n",llc.DSAP, llc.SSAP, llc.CR, llc.IG, llc.Control)
+	fmt.Printf("LLC/SNAP packet from %s to %s \n", srcMAC, dstMAC)
+	fmt.Printf("\tDSAP: %d SSAP:%d CR:%t IG:%t  Control: %d\n", llc.DSAP, llc.SSAP, llc.CR, llc.IG, llc.Control)
 	if ok {
-		d.snap = true;
-		fmt.Printf("\tOrg: %x (%s)  Type: %s\n", snap.OrganizationalCode, getOrgName(snap.OrganizationalCode),  snap.Type.String())
+		d.snap = true
+		fmt.Printf("\tOrg: %x (%s)  Type: %s\n", snap.OrganizationalCode, getOrgName(snap.OrganizationalCode), snap.Type.String())
 	}
 }
 
@@ -373,43 +366,42 @@ func PrintIPv4Info(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 	if !ip.SrcIP.IsUnspecified() {
 		d.IPv4 = ip.SrcIP
 	}
-	
+
 	switch ip.Protocol {
 
 	case layers.IPProtocolICMPv4:
 
 		icmpLayer := packet.Layer(layers.LayerTypeICMPv4)
-		fmt.Printf("ICMP packet from %s / %s to %s / %s\n",ip.SrcIP, srcMAC, ip.DstIP, dstMAC)
+		fmt.Printf("ICMP packet from %s / %s to %s / %s\n", ip.SrcIP, srcMAC, ip.DstIP, dstMAC)
 		if icmpLayer != nil {
 			icmp, _ := icmpLayer.(*layers.ICMPv4)
-			fmt.Printf("\t%s\n",icmp.TypeCode)
+			fmt.Printf("\t%s\n", icmp.TypeCode)
 		}
 		return
-		
+
 	case layers.IPProtocolUDP:
-	
+
 		dhcpLayer := packet.Layer(layers.LayerTypeDHCPv4)
 		if dhcpLayer != nil {
 			dhcp, _ := dhcpLayer.(*layers.DHCPv4)
 
 			hostname := ""
-			for _,o := range dhcp.Options {
+			for _, o := range dhcp.Options {
 				if o.Type == layers.DHCPOptHostname {
 					hostname = string(o.Data)
 				}
 			}
 
-			fmt.Printf("DHCP %s ",dhcp.Operation)
+			fmt.Printf("DHCP %s ", dhcp.Operation)
 			fmt.Printf("from %s / %s to %s ", ip.SrcIP, srcMAC, ip.DstIP)
 			fmt.Printf("hostname: %s\n", hostname)
 
-			
 			if hostname == "" {
-				fmt.Println(" %q",dhcp.Options)
+				fmt.Println(" %q", dhcp.Options)
 			} else {
 				d.clientID = hostname
 			}
-			
+
 			return
 		}
 
@@ -422,7 +414,7 @@ func PrintIPv4Info(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 				fmt.Printf("UDP packet from %s:%d to %s:%d\n ", ip.SrcIP, udp.SrcPort, ip.DstIP, udp.DstPort)
 
 				if udp.DstPort == 5353 {
-					if !dumpBonjour(d,packet) {
+					if !dumpBonjour(d, packet) {
 						applicationLayer := packet.ApplicationLayer()
 						if applicationLayer != nil {
 							Hexdump(applicationLayer.Payload())
@@ -434,7 +426,7 @@ func PrintIPv4Info(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 				} else if udp.DstPort == 137 {
 					applicationLayer := packet.ApplicationLayer()
 					if applicationLayer != nil {
-						name,code := extractNetBiosName(applicationLayer.Payload())
+						name, code := extractNetBiosName(applicationLayer.Payload())
 						if name != "" {
 							d.NBNames[name] = code
 						} else {
@@ -442,7 +434,7 @@ func PrintIPv4Info(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 							Hexdump(applicationLayer.Payload())
 						}
 					}
-					
+
 				} else {
 					applicationLayer := packet.ApplicationLayer()
 					if applicationLayer != nil {
@@ -457,7 +449,7 @@ func PrintIPv4Info(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 		return
 
 	case layers.IPProtocolTCP:
-		
+
 		// Honestly, for multicast, we should NEVER see TCP...
 		tcpLayer := packet.Layer(layers.LayerTypeTCP)
 		if tcpLayer != nil {
@@ -477,12 +469,12 @@ func PrintIPv4Info(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 		igmpLayer := packet.Layer(layers.LayerTypeIGMP)
 		if igmpLayer != nil {
 			igmp, ok := igmpLayer.(*layers.IGMP)
-			if (ok) {
-				fmt.Printf("%s from %s to %s \n",igmp.Type,ip.SrcIP,igmp.GroupAddress)
+			if ok {
+				fmt.Printf("%s from %s to %s \n", igmp.Type, ip.SrcIP, igmp.GroupAddress)
 			} else {
 				igmp, ok := igmpLayer.(*layers.IGMPv1or2)
-				if (ok) {
-					fmt.Printf("%s from %s to %s \n",igmp.Type,ip.SrcIP,igmp.GroupAddress)
+				if ok {
+					fmt.Printf("%s from %s to %s \n", igmp.Type, ip.SrcIP, igmp.GroupAddress)
 				} else {
 					fmt.Println("Huh?")
 					DumpUnknown(packet)
@@ -490,17 +482,15 @@ func PrintIPv4Info(d *Device, packet gopacket.Packet, srcMAC, dstMAC string) {
 			}
 		}
 
-
 		return
-		
-	default:	
+
+	default:
 		// If we get here, it's because we ran into something we didn't handle yet.
 
 		DumpUnknown(packet)
 		// Iterate over all layers, printing out each layer type
 	}
 }
-
 
 func DumpUnknown(packet gopacket.Packet) {
 
@@ -515,33 +505,33 @@ func DumpUnknown(packet gopacket.Packet) {
 }
 
 func Hexdump(data []byte) {
-        for i := 0; i < len(data); i += 16 {
-                Dumpline(uint32(i), data[i:min(i+16, len(data))])
-        }
+	for i := 0; i < len(data); i += 16 {
+		Dumpline(uint32(i), data[i:min(i+16, len(data))])
+	}
 }
 
 func Dumpline(addr uint32, line []byte) {
-        fmt.Printf( "\t0x%04x: ", int32(addr))
-        var i uint16
-        for i = 0; i < 16 && i < uint16(len(line)); i++ {
-                if i%2 == 0 {
-                        os.Stdout.WriteString(" ")
-                }
-                fmt.Printf( "%02x", line[i])
-        }
-        for j := i; j <= 16; j++ {
-                if j%2 == 0 {
-                        os.Stdout.WriteString(" ")
-                }
-                os.Stdout.WriteString("  ")
-        }
-        os.Stdout.WriteString("  ")
-        for i = 0; i < 16 && i < uint16(len(line)); i++ {
-                if line[i] >= 32 && line[i] <= 126 {
-                        fmt.Printf("%c", line[i])
-                } else {
-                        os.Stdout.WriteString(".")
-                }
-        }
-        os.Stdout.WriteString("\n")
+	fmt.Printf("\t0x%04x: ", int32(addr))
+	var i uint16
+	for i = 0; i < 16 && i < uint16(len(line)); i++ {
+		if i%2 == 0 {
+			os.Stdout.WriteString(" ")
+		}
+		fmt.Printf("%02x", line[i])
+	}
+	for j := i; j <= 16; j++ {
+		if j%2 == 0 {
+			os.Stdout.WriteString(" ")
+		}
+		os.Stdout.WriteString("  ")
+	}
+	os.Stdout.WriteString("  ")
+	for i = 0; i < 16 && i < uint16(len(line)); i++ {
+		if line[i] >= 32 && line[i] <= 126 {
+			fmt.Printf("%c", line[i])
+		} else {
+			os.Stdout.WriteString(".")
+		}
+	}
+	os.Stdout.WriteString("\n")
 }
